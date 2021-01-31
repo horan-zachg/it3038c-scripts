99a6fba7be2b50e4f3cbc95ea30bc09f16a8790f
uc-glossozj <glossozj@mail.uc.edu>
2021-01-31 15:48:57 -0500

    sysinfo.ps1

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,14 @@
+﻿#$Hello = "Hello, Powershell!"
+#Write-host($Hello)
+function getIP{
+    (Get-NetIPAddress).IPv4address | select-string "192*"
+}
+$IP = getIP
+write-host("This machine's IP is {0}" -f $IP)
+write-host("User is $env:username")
+$HOSTNAME = hostname
+write-host("Hostname is $HOSTNAME")
+$version = $PSVersionTable.PSVersion | Sort-Object major | foreach-object {$_.major}
+write-host("Powershell Version $version")
+$DATE = Get-Date -Format "dddd MMMM/dd/yyyy"
+write-host("Today's date is $DATE")

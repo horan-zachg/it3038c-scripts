8429e39e388790ed8f8e589bd9a408b98b0d0a49
uc-glossozj <glossozj@mail.uc.edu>
2021-02-14 23:14:45 -0500

    Guess the random Number between 1 and 15

--- Labs/Lab5.py
+++ Labs/Lab5.py
@@ -6,19 +6,19 @@ win = False
 turns = 1
 r = range (0,16)
 while win==False:
-    number1 = input("Enter a number between 1 and 15: ")
+    number1 = input("Enter a whole number between 1 and 15: ")
     number = int(number1)
     if number not in r:
-        print("Please select a number between 1 and 15 and try again")
+        print("Please enter a whole number between 1 and 15 and try again")
     else:
         if rn == number:
             win = True
-            print("You guessed the correct number!", rn)
-            print('You guessed the correct number in:',turns,'turns')
+            print("You guessed the correct whole number!", rn)
+            print('You guessed the correct whole number in:',turns,'turns')
             break
         else:
             turns += 1
             if rn < number:
                 print("Your guess was too high, please try again")
             else:
-                print("Your guess was too low, please try again")    
+                print("Your guess was too low, please try again")  

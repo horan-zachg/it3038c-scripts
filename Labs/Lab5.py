cc58cd7e6e26ec6c28703ee58f6887960aff66b9
uc-glossozj <glossozj@mail.uc.edu>
2021-02-14 20:15:59 -0500

    Lab: Guess the random Number between 1 - 15

---
+++ Labs/Lab5.py
@@ -0,0 +1,24 @@
+#First python lab
+#Guess the random Number between 1 - 15
+import random
+rn = random.randint(1,15)
+win = False
+turns = 1
+r = range (0,16)
+while win==False:
+    number1 = input("Enter a number between 1 and 15: ")
+    number = int(number1)
+    if number not in r:
+        print("Please select a number between 1 and 15 and try again")
+    else:
+        if rn == number:
+            win = True
+            print("You guessed the correct number!", rn)
+            print('You guessed the number in:',turns,'turns')
+            break
+        else:
+            turns += 1
+            if rn < number:
+                print("Your guess was too high, please try again")
+            else:
+                print("Your guess was too low, please try again")    

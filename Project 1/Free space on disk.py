7776ce8a8a3e474ba1ac802b436186dc95c2cac6
uc-glossozj <glossozj@mail.uc.edu>
2021-02-21 23:05:07 -0500

    Free space on disk

--- Project 1/Free space on disk.py
+++ Project 1/Free space on disk.py
@@ -1,3 +1,4 @@
+##This script allows the user to find out how many drive letters are on the pc including cloud storage devices and allows you to select a drive letter to find out how much space is remaining on the drive.
 import shutil
 import win32api
 def bytestogb(bytes):
@@ -15,8 +16,8 @@ def bytestogb(bytes):
             return "%.2f Megabytes" % (Megabytes)
     else:
         return "%.2f Kilobytes" % (Kilobytes)
-##The lines above were not written by me. The code has slight changes but is similar to the source
-#https://www.tutorialexample.com/a-simple-guide-to-python-get-disk-or-directory-total-space-used-space-and-free-space-python-tutorial/
+##The lines above were not written by me. The code has changes but the math is the same because you have to use this math to figure out the storage size remaining by calculating the kilobytes, megabytes, and gigabytes. I completely understand how this code works.
+##https://www.tutorialexample.com/a-simple-guide-to-python-get-disk-or-directory-total-space-used-space-and-free-space-python-tutorial/
 print("This script will let you know how much free space is remaining on a disk")
 print("Example disk letter input would be: C:\\")
 print("The disk letters that are on your computer is:")
@@ -26,7 +27,7 @@ print(drives)
 number1 = input("Enter your disk letter to see how much storage you have left on the drive: ")
 usage = shutil.disk_usage(number1)
 remaininggb = bytestogb(usage[2])
-##the drives declaration is also taken from snippets of code from stack overflow due to the drives command running from the import
+##The drives declaration is also taken from snippets of code from stack overflow due to the drives command running from the import.
 ##https://stackoverflow.com/questions/827371/is-there-a-way-to-list-all-the-available-drive-letters-in-python
 print('Your free space is:', remaininggb)
-##I decided to make this script because my pc currently has 9 drives and would be easier to check with this instead of disk management or file explorer
+##I decided to make this script because my pc currently has 9 drives and would be easier to check with this instead of disk management or file explorer.

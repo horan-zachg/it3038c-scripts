c9dbf9caedd36ca9f97893ef54ce2cbe1e98141e
uc-glossozj <glossozj@mail.uc.edu>
2021-02-21 21:27:45 -0500

    Free space on disk

--- Project 1/Free space on disk.py
+++ Project 1/Free space on disk.py
@@ -5,7 +5,7 @@ def formatSize(bytes):
         bytes = float(bytes)
         kb = bytes / 1024
     except:
-        return "Please enter a vlid disk letter"
+        return "Please enter a valid disk letter"
     if kb >= 1024:
         M = kb / 1024
         if M >= 1024:
@@ -15,8 +15,9 @@ def formatSize(bytes):
             return "%.2fM" % (M)
     else:
         return "%.2fkb" % (kb)
+#https://www.tutorialexample.com/a-simple-guide-to-python-get-disk-or-directory-total-space-used-space-and-free-space-python-tutorial/
 print("This script will let you know how much free space is remaining on a disk")
-print("Example disk letter input would be: c://")
+print("Example disk letter input would be: C:\\")
 print("The disk letters that are on your computer is:")
 drives = win32api.GetLogicalDriveStrings()
 drives = drives.split('\000')[:-1]
@@ -24,4 +25,5 @@ print(drives)
 number1 = input("Enter your disk letter to see how much storage you have left on the drive: ")
 usage = shutil.disk_usage(number1)
 free_space = formatSize(usage[2])
+##https://stackoverflow.com/questions/827371/is-there-a-way-to-list-all-the-available-drive-letters-in-python
 print('Your free space is:', free_space)

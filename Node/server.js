a2ac970800659a4256acccc1dfd11565d5dc017a
uc-glossozj <glossozj@mail.uc.edu>
2021-02-28 23:27:56 -0500

    Node

---
+++ Node/server.js
@@ -0,0 +1,43 @@
+var fs = require("fs");
+var http = require("http"); 
+var os = require("os");
+var ip = require("ip");
+
+var server = http.createServer(function(req, res) {
+    if (req.url === "/" ) {
+        fs.readFile("./public/index.html", "UTF-8", function(err, body){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(body);
+        });
+    }
+
+    else if(req.url.match("/sysinfo")) {
+        myHostName=os.hostname();
+        html=`
+        <!DOCTYPE html>
+        <head>
+            <title>Node JS Response</title>
+        </head>
+        <body>
+            <p>Hostname: ${myHostName}</p>
+            <p>IP: ${ip.address()}</p>
+            <p>Server Uptime: ${ip.address()}</p>
+            <p>Total Memory: ${ip.totalmemory()}</p>
+            <p>Free Memory: ${ip.freememory()}</p>
+            <p>Number of CPUs: ${ip.numberofcpus()}</p>
+        </body>
+        </html>
+        `
+        res.writeHead(200, {"Content-Type" : "text/html"});
+        res.end(html);
+    }
+
+    else {
+        res.writeHead(404, {"Content-Type":"text/plain"});
+        res.end(`404 File Not Found at ${req.url}`);
+    }
+
+});
+
+server.listen(3000);
+console.log("Server listening on port 3000");
